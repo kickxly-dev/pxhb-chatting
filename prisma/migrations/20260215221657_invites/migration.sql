42424be7c9d1fd05d0ff57afd229c6897846c50b
kickxly-dev <kickxly0@gmail.com>
2026-02-15 22:17:32 -0800

    prisma migration: invites

---
+++ prisma/migrations/20260215221657_invites/migration.sql
@@ -0,0 +1,22 @@
+﻿-- CreateTable
+CREATE TABLE "Invite" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "code" TEXT NOT NULL,
+    "serverId" TEXT NOT NULL,
+    "creatorId" TEXT NOT NULL,
+
+    CONSTRAINT "Invite_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Invite_code_key" ON "Invite"("code");
+
+-- CreateIndex
+CREATE INDEX "Invite_serverId_createdAt_idx" ON "Invite"("serverId", "createdAt");
+
+-- AddForeignKey
+ALTER TABLE "Invite" ADD CONSTRAINT "Invite_serverId_fkey" FOREIGN KEY ("serverId") REFERENCES "Server"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Invite" ADD CONSTRAINT "Invite_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
