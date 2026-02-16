3472f5f9f4fb300cd4925919d66fb7831ac2abde
kickxly-dev <kickxly0@gmail.com>
2026-02-15 21:22:29 -0800

    fix render deploy

--- server/index.js
+++ server/index.js
@@ -310,7 +310,7 @@ if (isProd) {
   const distPath = path.resolve(__dirname, '../dist')
 
   app.use(express.static(distPath))
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'))
   })
 }
